c620055c5c20d7a5608f4786baad2c010e7808b9
BT_1N3_17 <itsbenwu>
2019-02-25 08:37:40 -0500

    Need to do card class.

--- src/Card.java
+++ src/Card.java
@@ -37,7 +37,8 @@ public class Card {
          *                  containing the point value of the card
          */
         public Card(String cardRank, String cardSuit, int cardPointValue) {
-            /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+            this.rank=cardRank;
+            this.suit=cardSuit;
         }
 
 
