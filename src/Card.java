065e437141bc7496c6a6c8179e52fa76267cb5c7
BT_1N3_17 <itsbenwu>
2019-02-28 08:28:22 -0500

    Matches need fix

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,7 @@
 /**
  * Created by Teacher on 1/7/2019.
  */
-public class Card {
+public class    Card {
 
     /**
      * Card.java
@@ -29,70 +29,74 @@ public class Card {
         /**
          * Creates a new <code>Card</code> instance.
          *
-         * @param cardRank  a <code>String</code> value
-         *                  containing the rank of the card
-         * @param cardSuit  a <code>String</code> value
-         *                  containing the suit of the card
+         * @param cardRank       a <code>String</code> value
+         *                       containing the rank of the card
+         * @param cardSuit       a <code>String</code> value
+         *                       containing the suit of the card
          * @param cardPointValue an <code>int</code> value
-         *                  containing the point value of the card
+         *                       containing the point value of the card
          */
         public Card(String cardRank, String cardSuit, int cardPointValue) {
-            this.rank=cardRank;
-            this.suit=cardSuit;
+            Card card = new Card(cardRank,cardSuit,cardPointValue);
         }
 
 
         /**
          * Accesses this <code>Card's</code> suit.
+         *
          * @return this <code>Card's</code> suit.
          */
         public String suit() {
-            /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+            return suit;
         }
 
         /**
          * Accesses this <code>Card's</code> rank.
+         *
          * @return this <code>Card's</code> rank.
          */
         public String rank() {
-            /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+            return rank;
         }
 
         /**
          * Accesses this <code>Card's</code> point value.
+         *
          * @return this <code>Card's</code> point value.
          */
         public int pointValue() {
-            /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+            return pointValue;
         }
 
-        /** Compare this card with the argument.
+        /**
+         * Compare this card with the argument.
+         *
          * @param otherCard the other card to compare to this
          * @return true if the rank, suit, and point value of this card
-         *              are equal to those of the argument;
-         *         false otherwise.
+         * are equal to those of the argument;
+         * false otherwise.
          */
         public boolean matches(Card otherCard) {
-            /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+            if(otherCard.equals(Card.card)){
+                return true;
+            }
+            return false;
         }
 
         /**
          * Converts the rank, suit, and point value into a string in the format
-         *     "[Rank] of [Suit] (point value = [PointValue])".
+         * "[Rank] of [Suit] (point value = [PointValue])".
          * This provides a useful way of printing the contents
          * of a <code>Deck</code> in an easily readable format or performing
          * other similar functions.
          *
          * @return a <code>String</code> containing the rank, suit,
-         *         and point value of the card.
+         * and point value of the card.
          */
         @Override
         public String toString() {
-            /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+            return rank + " of " + suit + " point value = " + pointValue;
 
         }
+    }
 }
